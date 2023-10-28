e7ff5edeaa06080d312dbf1db6a4bf3ae0011e24
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-10-28 11:45:41 -0400

    rutas  y controladores

--- server/index.js
+++ server/index.js
@@ -1,111 +1,62 @@
+
+// ./server/index.js
+ 
 // 1. IMPORTACIONES
-
 // A. LIBRERÍAS
-
 import express from "express"
-
 import cors from "cors"
-
 import dotenv from "dotenv"
-
 import swaggerUI from "swagger-ui-express"
-
 import swaggerJSDoc from "swagger-jsdoc"
-
 import path, { dirname } from "path"
-
 import { fileURLToPath } from "url"
-
  
-
 // B. ARCHIVOS
-
 import userRoute from "./routes/users.js"
-
 import carRoute from "./routes/cars.js"
-
 import pizzaRoute from "./routes/pizzas.js"
-
+import toppingsRoute from "./routes/toppings.js"
 import connectDB from "./config/db.js"
-
  
-
 // 2. INICIALIZADORES
-
 const app = express()
-
 app.use(cors())
-
 app.use(express.json())
-
 dotenv.config()
-
  
-
 connectDB()
-
  
-
 const __filename = fileURLToPath(import.meta.url)
-
 const __dirname = dirname(__filename)
-
  
-
 const port = process.env.BASE_URL_PORT || 3005
-
  
-
 const swaggerOptions = {
-
   definition: {
-
     openapi: "3.0.0",
-
     info: {
-
       title: "Documentación sobre usuarios en Ucamp",
-
       version: "1.0.0",
-
     },
-
   },
-
   apis: [`${path.join(__dirname, "./routes/*.js")}`],
-
 }
-
  
-
 const swaggerDocs = swaggerJSDoc(swaggerOptions)
-
  
-
 // 3. RUTAS
-
  
-
 // A. APLICACIÓN
-
 // PROD: https://midominio.com/
-
 // DEV: localhost:3005/
-
 app.use("/api/v1/users", userRoute)
-
 app.use("/api/v1/cars", carRoute)
-
 app.use("/api/v1/pizzas", pizzaRoute)
-
+app.use("/api/v1/toppings", toppingsRoute)
  
-
 // B. DOCUMENTACIÓN
-
 app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocs))
-
  
-
 // 4. LEVANTAMIENTO DEL SERVIDOR
-
 app.listen(port, () => console.log("Servidor está activo."))
+ 
