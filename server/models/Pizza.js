2db08ca6b33a7f20fb3b0b45616528547a10bb71
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-11-04 16:05:18 -0400

    class4novrmbre

--- server/models/Pizza.js
+++ server/models/Pizza.js
@@ -1,33 +1,60 @@
+// models/Pizza.js
+ 
 // 1. IMPORTACIÓN
-
 import mongoose from "mongoose"
-
  
-
 // 2. SCHEMA
-
 const pizzaSchema = mongoose.Schema({
-
-  name: {
-
+  idStripe: {
     type: String,
-
     required: true,
-
   },
-
+  name: {
+    type: String,
+    required: true,
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  prices: [
+    {
+      id: {
+        type: String,
+        required: true,
+      },
+      size: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      priceDescription: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  img: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 })
-
  
-
 // 3. MODELO
-
 const Pizza = mongoose.model("Pizza", pizzaSchema)
-
  
-
 // 4. EXPORTACIÓN
-
 export default Pizza
-
  
---
+++ server/routes/chekout.js
@@ -0,0 +1,11 @@
+// server/routes/checkout.js
+ 
+import express from "express"
+import checkoutController from "../controllers/checkoutController.js"
+ 
+const router = express.Router()
+ 
+router.get("/create-checkout-session", checkoutController.createCheckoutSession)
+ 
+export default router
+ 
